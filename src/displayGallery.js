a64460cf0aedd0bfc69d0bf9fb84bf51b12690ea
Marina Ruiz <marruiart@hotmail.com>
2022-10-15 19:30:32 +0200

    improved design + active menu button

--- src/displayGallery.js
+++ src/displayGallery.js
@@ -8,6 +8,7 @@ export function displayOneCocktail(cocktail) {
     var card = document.createElement("div"),
         information = document.createElement("div"),
         infoText = document.createElement("div"),
+        divCocktailImg = document.createElement("div"),
         fieldStrDrink = document.createElement("h2"),
         fieldStrAlcoholic = document.createElement("p"),
         strInstructions = document.createElement("p"),
@@ -18,6 +19,8 @@ export function displayOneCocktail(cocktail) {
     fieldStrDrink.className = "cocktail_name --one_coctkail";
     fieldStrAlcoholic.className = "alcohol_info --one_coctkail";
     strInstructions.className = "instructions_info --one_coctkail";
+    infoText.className = "div--info_text --one_coctkail";
+    divCocktailImg.className = "div--cocktail_img --one_coctkail";
     cocktailImg.className = "cocktail_img --one_coctkail";
     favouriteButton.className = "button--favourite";
     information.className = "card__information";
@@ -25,18 +28,20 @@ export function displayOneCocktail(cocktail) {
     fieldStrAlcoholic.innerHTML = cocktail.strAlcoholic;
     strInstructions.innerHTML = cocktail.strInstructions;
     favouriteButton.innerHTML = '<i class="fa fa-heart"></i>';
-    cocktailImg.src = cocktail.strDrinkThumb;
-    cocktailImg.alt = cocktail.strAlcoholic;
+    divCocktailImg.style.backgroundImage = "url("+cocktail.strDrinkThumb+")";
+    //cocktailImg.src = cocktail.strDrinkThumb;
+    //cocktailImg.alt = cocktail.strAlcoholic;
     favouriteButton.addEventListener('click', function () {
         saveFavourite(cocktail.strDrink, cocktail.strAlcoholic, cocktail.strDrinkThumb, cocktail.strInstructions);
     })
 
     fieldStrDrink.appendChild(favouriteButton);
-    card.appendChild(fieldStrDrink);
-    information.appendChild(cocktailImg);
+    divCocktailImg.appendChild(cocktailImg);
     infoText.appendChild(fieldStrAlcoholic);
     infoText.appendChild(strInstructions);
+    information.appendChild(divCocktailImg);
     information.appendChild(infoText);
+    card.appendChild(fieldStrDrink);
     card.appendChild(information);
 
     CONTAINER.appendChild(card);
@@ -53,14 +58,15 @@ export function displayGallery(cocktailsByLetter) {
             favouriteButton = document.createElement("button");
 
         card.className = "card";
-        fieldStrDrink.innerHTML = cocktail.strDrink;
         fieldStrDrink.className = "cocktail_name";
-        fieldStrAlcoholic.innerHTML = cocktail.strAlcoholic;
-        cocktailImg.src = cocktail.strDrinkThumb;
         cocktailImg.className = "gallery_img";
-        cocktailImg.alt = cocktail.strAlcoholic;
-        favouriteButton.innerHTML = '<i class="fa fa-heart"></i>';
         favouriteButton.className = "button--favourite";
+        fieldStrAlcoholic.className = "cocktail_name --alcoholic";
+        fieldStrDrink.innerHTML = cocktail.strDrink;
+        fieldStrAlcoholic.innerHTML = cocktail.strAlcoholic;
+        favouriteButton.innerHTML = '<i class="fa fa-heart"></i>';
+        cocktailImg.src = cocktail.strDrinkThumb;
+        cocktailImg.alt = cocktail.strAlcoholic;
         favouriteButton.addEventListener('click', function () {
             saveFavourite(cocktail.strDrink, cocktail.strAlcoholic, cocktail.strDrinkThumb, cocktail.strInstructions);
         });
--- src/hideElements.js
+++ src/hideElements.js
@@ -3,11 +3,29 @@
 import { fillSelectOptions, resetSelect } from './selectFunctions.js';
 import { ABECEDARY } from './arrays.js';
 
-const MAIN_TABLE = document.getElementById("main_table");
-const SEARCH_MAIN = document.getElementById("search_main");
-const LETTER_TABLE = document.getElementById("letter_table");
+const MAIN_TABLE = document.getElementById("main_table"),
+    SEARCH_MAIN = document.getElementById("search_main"),
+    LETTER_TABLE = document.getElementById("letter_table");
+
+
+function activeMenuButton(id) {
+    let button = document.getElementById(id),
+        buttons = document.querySelectorAll(".button--menu");
+
+    console.log(button);
+    console.log(buttons);
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function () {
+            let current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
+    }
+}
 
 export function hideElements(id) {
+    activeMenuButton(id);
     if (id == "show_main_table") {
         LETTER_TABLE.style = "display:none";
         SEARCH_MAIN.style = "display:none";
